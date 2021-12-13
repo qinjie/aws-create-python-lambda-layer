829bb847f399631541a47a038cd1e90263feacf6
mark.qj <mark.qj@gmail.com>
2021-12-13 12:57:11 +0800

    Bug fix

--- create_lambda_layer.sh
+++ create_lambda_layer.sh
@@ -81,4 +81,4 @@ rm -rf v-env
 
 # # Publish it to AWS
 #echo "Create lambda layer..."
-#aws lambda pish-layer-version --layer-name $FILEBASENAME --zip-file fileb://$ZIP_FILE --compatible-runtimes $PYTHON_VERSION
+#aws lambda push-layer-version --layer-name $FILEBASENAME --zip-file fileb://$ZIP_FILE --compatible-runtimes $PYTHON_VERSION
